92ca6486435477d0e49e7d25ebe55c9a98a98d79
Niraj <niraj.mulji@gmail.com>
2016-06-01 14:31:36 -0500

    This is my updated resume and portfolio exercise for blade.

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -1,7 +1,132 @@
 @extends('layouts.master')
 
+    <!--Resume Style Sheet-->
+
+    <link rel="stylesheet" href="/css/resume.css">
+
 @section('content')
 
-<p>Yo this is my resume!</p>
+	<div id="mycontainer" class="text-center">
+
+		<div class="row">
+			<div class="categories">
+			  <h1>Education</h1>
+			</div>
+		</div>
+
+		<div class="row" id="school">
+			<div class="container" id="job_one">
+				<p class="jobs">
+					Southern Methodist University, Simmons School of Education                                    
+				</p>
+				<p class="positions">
+					Bachelor of Applied Physiology & Sport Management
+				</p>
+		</div>
+
+		<div class="location_dates">
+			<div class="locations">
+				Dallas, TX
+			</div>
+
+			<div class="dates">
+				May 2014
+			</div>
+
+		<div class="row">
+			<ul>
+				<li>Honor Roll Award for academic excellence</li>
+		</div>
+
+
+		<div class="row">
+			<div class="categories">
+			  <h1>Work Experience</h1>
+			</div>
+		</div>
+
+		<div class="row" id="spurs">
+			<div class="container" id="job_one">
+				<p class="jobs">
+					Spurs Sports & Entertainment, LLC – San Antonio Spurs                                     
+				</p>
+				<p class="positions">
+					Basketball Operations Quality Assurance Assistant II (Front Office)
+				</p>
+		</div>
+
+		<div class="location_dates">
+			<div class="locations">
+				San Antonio, TX
+			</div>
+
+			<div class="dates">
+				May 2014 – Present
+			</div>
+
+		<div class="row">
+			<ul>
+				<li>Build tools to sequence NBA player contracts and ensure efficiency around NBA salary cap and basketball strategy</li>
+				<li>Organize and enter scouting and basketball information into databases through front-end applications</li>
+				<li>Complete special projects as assigned by In-House Legal Counsel, Assistant Coaches, and General Manager</li>
+				<li>Assist in evaluation of NBA draft prospects through on-site scouting and intel gathering</li>
+		</div>
+
+		<div class="row" id="SMU">
+			<div class="container" id="job_one">
+				<p class="jobs">
+					Southern Methodist University – Men's Basketball               
+				</p>
+				<p class="positions">
+					Head Student Manager and Assistant Video Coordinator
+				</p>
+		</div>
+
+		<div class="location_dates">
+			<div class="locations">
+				Dallas, TX
+			</div>
+
+			<div class="dates">
+				May 2012 – May 2014
+			</div>
+		</div>
+			
+		<div class="row">
+			<ul>
+				<li>Coded and organized practice and game film for analysis and scouting purposes using Sports Code Elite</li>
+				<li>Installed new technology to practice courts and video room, including backboard cameras and data storage</li>
+				<li>Assisted in preparation of scouting reports and video using Synergy Sports Technology</li>
+				<li>Led a team comprised of 8 undergraduate student managers</li>
+		</div>
+
+		<div class="row" id="pacers">
+			<div class="container" id="job_one">
+				<p class="jobs">
+					Pacers Sports & Entertainment, LLC – Indiana Pacers               
+				</p>
+				<p class="positions">
+					Basketball Operations Intern (Front Office)
+				</p>
+		</div>
+
+		<div class="location_dates">
+			<div class="locations">
+				Indianapolis, IN
+			</div>
+
+			<div class="dates">
+				May 2013 – Aug 2013
+			</div>
+		</div>
+			
+		<div class="row">
+			<ul>
+				<li>Assisted in NBA draft preparation by collecting and organizing scouting reports, as well as preparing draft books</li>
+				<li>Completed various projects involving the NBA salary cap, Collective Bargaining Agreement, and trade scenarios</li>
+				<li>Projected 5 year financial reports of all NBA teams, including player rotation analysis, using RealGM</li>
+				<li>Performed daily basketball operations to ensure efficiency of tasks and that deadlines were met</li>
+		</div>
+	</div>
 
 @stop
